309f124440139deb21c8f91b45ab788d1e84aac6
Dafna <Dafna@192.168.1.132>
2017-05-25 22:03:24 -0400

    First commit.

---
+++ src/vip4_userstorytesting/Data.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vip4_userstorytesting;
+
+/**
+ *
+ * @author Dafna
+ */
+public interface Data {
+    
+    final String PROF_EMAIL = "";
+    final String PROF_PW = "";  
+    final String USER_EMAIL = ""; 
+    final String USER_PW = ""; 
+}
